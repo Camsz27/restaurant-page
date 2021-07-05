a65c77476bc9c082207e83b193d8c082ae261c91
Camsz27 <camiloagr2707@gmail.com>
2021-07-04 20:59:31 -0500

    Header of site done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("here we go");
+import "./style.css";
+import Icon from "./logo.png";
+
+/* const container = document.querySelector("div");
+const image = document.createElement("img");
+image.setAttribute("src", Icon);
+image.setAttribute("alt", "salvaje-logo");
+container.append(image); */
