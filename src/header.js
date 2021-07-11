20be721ebabfc7f590932e51d0740717d1c12ab0
Camsz27 <camiloagr2707@gmail.com>
2021-07-11 17:13:46 -0500

    Now each tab changes appropiately

--- src/header.js
+++ src/header.js
@@ -1,4 +1,7 @@
 import Icon from "./Images/logo.png";
+import restaurant from "./restaurant";
+import menu from "./menu";
+import contact from "./contact";
 
 // Creates header of the page
 const header = () => {
@@ -12,7 +15,7 @@ const header = () => {
   div.append(logo);
   const headerList = document.createElement("ul");
   const restauranteHeader = document.createElement("li");
-  restauranteHeader.classList.add("header");
+  restauranteHeader.classList.add("header", "active");
   restauranteHeader.textContent = "El Restaurante";
   const menuHeader = document.createElement("li");
   menuHeader.classList.add("header");
@@ -21,8 +24,24 @@ const header = () => {
   contactoHeader.classList.add("header");
   contactoHeader.textContent = "Contacto";
   headerList.append(restauranteHeader, menuHeader, contactoHeader);
+  restauranteHeader.addEventListener("click", removeContent);
+  restauranteHeader.addEventListener("click", restaurant);
+  menuHeader.addEventListener("click", removeContent);
+  menuHeader.addEventListener("click", menu);
+  contactoHeader.addEventListener("click", removeContent);
+  contactoHeader.addEventListener("click", contact);
   div.append(headerList);
   headerContainer.append(div);
+  function removeContent() {
+    let divLength = headerContainer.childNodes.length;
+    restauranteHeader.classList.remove("active");
+    menuHeader.classList.remove("active");
+    contactoHeader.classList.remove("active");
+    this.classList.add("active");
+    for (let i = 3; i <= divLength; i++) {
+      headerContainer.childNodes[3].remove();
+    }
+  }
 };
 
 export default header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import "./style.css";
 import header from "./header";
 import restaurant from "./restaurant";
-import menu from "./menu";
-import contact from "./contact";
 
 // Creates header
 header();
 // Creates content of the landing page
-//restaurant();
-//menu();
-contact();
+restaurant();
